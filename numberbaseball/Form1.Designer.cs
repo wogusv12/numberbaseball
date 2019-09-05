1cf1a596dbffa9a127ddf2c508fd51e8002c6892
wogusv12 <wogusv12@naver.com>
2019-09-06 01:55:07 +0900

    0strike 2ball

---
+++ numberbaseball/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace numberbaseball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.number = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.strike = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ball = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.number,
+            this.strike,
+            this.ball});
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.ShowItemToolTips = true;
+            this.listView1.Size = new System.Drawing.Size(281, 419);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // number
+            // 
+            this.number.Text = "숫자";
+            this.number.Width = 108;
+            // 
+            // strike
+            // 
+            this.strike.Text = "스트라이크";
+            this.strike.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.strike.Width = 76;
+            // 
+            // ball
+            // 
+            this.ball.Text = "볼";
+            this.ball.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(308, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "숫자 입력";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(371, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(299, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "스트라이크";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(371, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(347, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "볼";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(371, 94);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(320, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "입력";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(470, 134);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "초기화";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(320, 185);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView2.Location = new System.Drawing.Point(310, 224);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(235, 207);
+            this.listView2.TabIndex = 10;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "예상 결과값";
+            this.columnHeader1.Width = 228;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 445);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader number;
+        private System.Windows.Forms.ColumnHeader strike;
+        private System.Windows.Forms.ColumnHeader ball;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ numberbaseball/Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace numberbaseball
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ListViewItem newitem = new ListViewItem(new String[] { textBox1.Text, textBox2.Text, textBox3.Text });
+            listView1.Items.Add(newitem);
+
+            check(textBox1.Text, Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+
+        public void check(String num, int strike, int ball)
+        {
+            String[] var;
+            if (listView2.Items.Count >= 1)
+            {
+                var = new String[listView2.Items.Count];
+                for (int i = 0; i < var.Length; i++)
+                {
+                    var[i] = listView2.Items[i].Text;
+                }
+                listView2.Items.Clear();
+            } else
+            {
+                var = new String[876];
+
+                for (int i = 0; i < 876; i++)
+                {
+                    var[i] = Convert.ToString(i + 123);
+                }
+
+            }
+            MessageBox.Show("strike = " + strike + " ball = " + ball);
+
+            char[] char_num = num.ToArray();
+            if(strike != 0 || ball != 0)
+            {
+                
+                if(strike ==0)
+                {
+                    if (ball == 1) {
+                        for(int i = 0; i<var.Length; i++)
+                        {
+                            char[] var_array = var[i].ToArray();
+                            //1번째자리에 a x 2번째자리에 b x 세번째자리에 c X
+                            if (char_num[0] != var_array[0] && char_num[1] != var_array[1] && char_num[2] != var_array[2])
+                            {
+                                //2번째 3번째 자리에 a 일때
+                                if(char_num[0]== var_array[1] || char_num[0] == var_array[2])
+                                {
+                                    //b가 1번째,3번째값이 아니고 c가 1,2번째가 아닐때
+                                    if(char_num[1] !=var_array[0] && char_num[1] != var_array[2] && char_num[2] != var_array[0] && char_num[2] != var_array[1])
+                                    {
+                                        listView2.Items.Add(var[i]);
+                                    }
+
+                                }else if(char_num[1] == var_array[0] || char_num[1] == var_array[2]) //1, 3번째자리에 b일때
+                                {    // a가 2,3번째가 아니고 c가 1,2번째가 아닐때
+                                    if (char_num[0] != var_array[1] && char_num[0] != var_array[2] && char_num[2] != var_array[0] && char_num[2] != var_array[1])
+                                    {
+                                        listView2.Items.Add(var[i]);
+                                    }
+                                }else if(char_num[2] == var_array[0] || char_num[2] == var_array[1])
+                                {
+                                    if (char_num[0] != var_array[1] && char_num[0] != var_array[2] && char_num[1] != var_array[0] && char_num[1] != var_array[2])
+                                    {
+                                        listView2.Items.Add(var[i]);
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+                    else if (ball == 2) {
+                        for (int i =0; i< var.Length; i++)
+                        {
+                            char[] var_array = var[i].ToArray();
+                            //1번째자리에 a x 2번째자리에 b x 세번째자리에 c X
+                            if (char_num[0] != var_array[0] && char_num[1] != var_array[1] && char_num[2] != var_array[2])
+                            {
+                                //a가 2번째 자리일때
+                                if(char_num[0] == var_array[1])
+                                {
+                                    //b가 1번째 자리일때
+                                    if (char_num[1] == var_array[0])
+                                    {
+                                        if(char_num[2] != var_array[2])
+                                        {
+                                            listView2.Items.Add(var[i]);
+                                        }
+                                        //b가 3번째 자리일때
+                                    }else if(char_num[1] == var_array[2])
+                                    {
+                                        if(char_num[2] != var_array[0])
+                                        {
+                                            listView2.Items.Add(var[i]);
+                                        }
+                                        //c 가 첫번째 자리였을때
+                                    }else if(char_num[2] == var_array[0])
+                                    {
+                                        if(char_num[1] != var_array[2])
+                                        {
+                                            listView2.Items.Add(var[i]);
+                                        }
+
+                                    }
+                                    
+                                } // a가 3번째 자리일때
+                                else if(char_num[0] == var_array[2])
+                                {
+                                    //b가 1번째 자리일때
+                                    if (char_num[1] == var_array[0])
+                                    {
+                                        if (char_num[2] != var_array[2])
+                                        {
+                                            listView2.Items.Add(var[i]);
+                                        }
+                                      
+                                    }
+
+                                    //c가 1번째 자리일때
+                                    else if (char_num[2] == var_array[0])
+                                    {
+                                        if (char_num[1] != var_array[1])
+                                        {
+                                            listView2.Items.Add(var[i]);
+                                        }
+                                     
+                                    }
+
+                                    //c가 2번째 자리일때
+                                    else if (char_num[2] == var_array[1])
+                                    {
+                                        if (char_num[1] != var_array[0])
+                                        {
+                                            listView2.Items.Add(var[i]);
+                                        }
+
+                                    }
+
+                                }//b가 1번째 자리일때
+                                else if(char_num[1] == var_array[0])
+                                {
+                                    if (char_num[0] == var_array[1])
+                                    {
+                                        if (char_num[2] != var_array[2])
+                                        {
+                                            listView2.Items.Add(var[i]);
+                                        }
+                                   
+                                    }
+                                    else if (char_num[0] == var_array[2])
+                                    {
+                                        if (char_num[2] != var_array[1])
+                                        {
+                                            listView2.Items.Add(var[i]);
+                                        }
+                                    
+                                    }
+                                    else if (char_num[2] == var_array[1])
+                                    {
+                                        if (char_num[0] != var_array[3])
+                                        {
+                                            listView2.Items.Add(var[i]);
+                                        }
+
+                                    }
+
+                                }
+                                //b가 3번째 자리일때 
+                                else if(char_num[1] == var_array[2])
+                                {
+                                    if (char_num[0] == var_array[1])
+                                    {
+                                        if (char_num[2] != var_array[0])
+                                        {
+                                            listView2.Items.Add(var[i]);
+                                        }
+                                    }
+                                    else if (char_num[2] == var_array[0])
+                                    {
+                                        if (char_num[0] != var_array[1])
+                                        {
+                                            listView2.Items.Add(var[i]);
+                                        }
+                                       
+                                    }
+                                    else if (char_num[2] == var_array[1])
+                                    {
+                                        if (char_num[0] != var_array[0])
+                                        {
+                                            listView2.Items.Add(var[i]);
+                                        }
+
+                                    }
+                                }//c가 1번째 자리일때
+                                else if(char_num[2] == var_array[0])
+                                {
+                                    if (char_num[0] == var_array[1])
+                                    {
+                                        if (char_num[1] != var_array[2])
+                                        {
+                                            listView2.Items.Add(var[i]);
+                                        }
+                                   
+                                    }
+                                    else if (char_num[0] == var_array[2])
+                                    {
+                                        if (char_num[2] != var_array[2])
+                                        {
+                                            listView2.Items.Add(var[i]);
+                                        }
+                                       
+                                    }
+                                    else if (char_num[1] == var_array[2])
+                                    {
+                                        if (char_num[0] != var_array[1])
+                                        {
+                                            listView2.Items.Add(var[i]);
+                                        }
+
+                                    }
+
+                                }//c가 2번째 자리일때
+                                else if(char_num[2] == var_array[1])
+                                {
+                                    if (char_num[0] == var_array[2])
+                                    {
+                                        if (char_num[1] != var_array[2])
+                                        {
+                                            listView2.Items.Add(var[i]);
+                                        }
+                                        //b가 3번째 자리일때
+                                    }
+                                    else if (char_num[1] == var_array[0])
+                                    {
+                                        if (char_num[0] != var_array[2])
+                                        {
+                                            listView2.Items.Add(var[i]);
+                                        }
+                                        //c 가 첫번째 자리였을때
+                                    }
+                                    else if (char_num[1] == var_array[2])
+                                    {
+                                        if (char_num[0] != var_array[0])
+                                        {
+                                            listView2.Items.Add(var[i]);
+                                        }
+
+                                    }
+
+                                }
+
+                            }
+
+                        }
+
+                    }
+                    else if (ball == 3) { }
+
+                }else if(strike ==1)
+                {
+                    if (ball == 0) { }
+                    else if (ball == 1) { }
+                    else if (ball == 2) { }
+                }
+                else if(strike == 2)
+                {
+                    if (ball == 0) { }
+                    else if (ball == 1) { }
+
+                }
+                else { listView2.Items.Add(num); }
+            }else
+            {
+                MessageBox.Show("no strike no ball");
+            }
+            
+
+        }
+    }
+}
